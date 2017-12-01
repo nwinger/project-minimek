d1a5925c2dff3c59db95691d95844165d419932d
Nicklas Pouey-Winger <nicklas.pouey-winger@evry.com>
2017-12-01 13:11:42 +0100

    Create a TabBar component to render a list of tabs

---
+++ src/features/tabs/Tab.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Menu } from "semantic-ui-react";
+
+const Tab = ({ name, label, onClick, active }) => {
+	return <Menu.item name={name} content={label} active={active} onClick={() => onClick(name)} />;
+};
+
+export default Tab;
