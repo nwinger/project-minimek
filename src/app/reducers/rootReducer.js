682cf81d04a13741333981c3ce87f8bbf7e92d9e
Nicklas Pouey-Winger <nicklas.pouey-winger@evry.com>
2017-12-06 08:55:49 +0100

    Add initial unit info reducer and connection

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 
 import tabsReducer from "features/tabs/tabsReducer";
+import unitInfoReducer from "features/unitInfo/unitInfoReducer";
 
 const rootReducer = combineReducers({
+	unitInfo: unitInfoReducer,
 	tabs: tabsReducer
 });
 
