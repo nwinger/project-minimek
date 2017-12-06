390b94fc87d2c5c8cc08ea4d8195ffba4962d4d4
Nicklas Pouey-Winger <nicklas.pouey-winger@evry.com>
2017-12-06 08:03:37 +0100

    Extract components from Pilots panel, and add initial test data

---
+++ src/features/pilots/PilotsList/PilotsList.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Table } from "semantic-ui-react";
+
+import PilotsListHeader from "./PilotsListHeader";
+import PilotsListRow from "./PilotsListRow";
+
+class PilotsList extends Component {
+	render() {
+		const { pilots } = this.props;
+
+		const pilotRows = pilots.map(pilot => {
+			return <PilotsListRow pilot={pilot} key={pilot.name} />;
+		});
+
+		return (
+			<Table celled>
+				<PilotsListHeader />
+				<Table.Body>{pilotRows}</Table.Body>
+			</Table>
+		);
+	}
+}
+
+export default PilotsList;
