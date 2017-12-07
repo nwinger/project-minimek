42677beaf9106cd90f60c9a731ff68e068224fa5
Nicklas Pouey-Winger <winger.nicklas@gmail.com>
2017-12-07 07:50:53 +0100

    Set up Redux-ORM and add an initial entities reducer

---
+++ src/app/reducers/entitiesReducer.js
@@ -0,0 +1,7 @@
+import { createReducer } from "common/utils/reducerUtils";
+
+import orm from "app/orm";
+
+const initialState = orm.getEmptyState();
+
+export default createReducer(initialState, {});
